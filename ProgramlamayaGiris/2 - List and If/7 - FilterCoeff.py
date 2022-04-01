b66995bceed4cef778415846d030be6bbbcf2d0c
Malik Enes Safak <e.maliksafak@gmail.com>
2022-04-01 11:37:30 +0300

    coefficients

--- ProgramlamayaGiris/2 - List and If/7 - FilterCoeff.py
+++ ProgramlamayaGiris/2 - List and If/7 - FilterCoeff.py
@@ -5,7 +5,7 @@ samplerate = 44100
 duration = 1.0
 total_num_of_samples = int(samplerate * duration)
 amplitude = 1.0
-filter = [-0.000716, -0.010806, -0.010580, 0.078926, 0.260783, 0.362812, 0.260783, 0.078926, -0.010580, -0.010806, -0.000716]
+coefficients = [-0.000716, -0.010806, -0.010580, 0.078926, 0.260783, 0.362812, 0.260783, 0.078926, -0.010580, -0.010806, -0.000716]
 
 time = []
 sinewave = []
@@ -18,20 +18,20 @@ for i in range(total_num_of_samples):
 #########################################
 
 for i in range(total_num_of_samples):
-    if i < len(filter):
+    if i < len(coefficients):
         filtered[i] = sinewave[i]
-    else:
-        filtered[i] += sinewave[i - 0] * filter[0]
-        filtered[i] += sinewave[i - 1] * filter[1]
-        filtered[i] += sinewave[i - 2] * filter[2]
-        filtered[i] += sinewave[i - 3] * filter[3]
-        filtered[i] += sinewave[i - 4] * filter[4]
-        filtered[i] += sinewave[i - 5] * filter[5]
-        filtered[i] += sinewave[i - 6] * filter[6]
-        filtered[i] += sinewave[i - 7] * filter[7]
-        filtered[i] += sinewave[i - 8] * filter[8]
-        filtered[i] += sinewave[i - 9] * filter[9]
-        filtered[i] += sinewave[i - 10] * filter[10]
+    else: 
+        filtered[i] = sinewave[i - 0] * coefficients[0]
+        filtered[i] += sinewave[i - 1] * coefficients[1]
+        filtered[i] += sinewave[i - 2] * coefficients[2]
+        filtered[i] += sinewave[i - 3] * coefficients[3]
+        filtered[i] += sinewave[i - 4] * coefficients[4]
+        filtered[i] += sinewave[i - 5] * coefficients[5]
+        filtered[i] += sinewave[i - 6] * coefficients[6]
+        filtered[i] += sinewave[i - 7] * coefficients[7]
+        filtered[i] += sinewave[i - 8] * coefficients[8]
+        filtered[i] += sinewave[i - 9] * coefficients[9]
+        filtered[i] += sinewave[i - 10] * coefficients[10]
 
 #########################################
 

5d9f2f806d0a8b2340aea59bd33f8717d3a773c8
Malik Enes Şafak <e.maliksafak@gmail.com>
2022-06-14 02:13:35 +0300

    ProgGiriş Final

---
+++ ProgramlamayaGiris/Final.py
@@ -0,0 +1,24 @@
+import math
+import random
+import matplotlib.pyplot as plt
+
+# Fonksiyonlarınızı bu alanın altına yerleştirin
+
+
+
+# Fonksiyonlarınızı bu alanın üstüne yerleştirin
+
+samplerate = 44100
+frekans_1 = 3
+frekans_2 = 5
+mix = [0] * samplerate
+
+# Ana programınızı bu alanın altına yerleştirin
+
+
+
+# Ana programınızı bu alanın üstüne yerleştirin
+# Son hesapladığınız sinyali mix listesinin içine yerleştirin
+
+plt.plot(mix)
+plt.show()
